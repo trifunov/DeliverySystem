e100fc91e5c4925dc91ea0a59edb901611c4cc31
Panche Trifunov <panche@servsuite.net>
2020-01-23 00:05:13 +0100

    no https

--- DeliverySystem.Web/Startup.cs
+++ DeliverySystem.Web/Startup.cs
@@ -54,10 +54,10 @@ namespace DeliverySystem.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
